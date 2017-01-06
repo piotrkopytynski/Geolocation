d077259069762d6048a12b0309d006f9ffdf449b
Piotr Bednarek <piotrekbe93@gmail.com>
2017-01-06 15:49:55 +0100

    added maps

--- src/main/java/pl/geolocal/main/MainWindow.java
+++ src/main/java/pl/geolocal/main/MainWindow.java
@@ -9,11 +9,10 @@ import pl.geolocal.service.DistanceService;
 import pl.geolocal.service.GeolocationService;
 import pl.geolocal.util.GeolocationOperations;
 import pl.geolocal.util.IpValidator;
+import pl.geolocal.util.MapGenerator;
 
 import javax.annotation.PostConstruct;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 /**
@@ -27,22 +26,55 @@ public class MainWindow extends JFrame {
     private final GeolocationService geolocationService;
     private final DistanceService distanceService;
     private final IpValidator ipValidator;
+    private final MapGenerator mapGenerator;
+
+    private String localIpAddress;
+    private Geolocation geolocationLocal;
 
     private JPanel rootPanel;
-    private JTabbedPane tabbedPane1;
-    private JTextField remoteIPtextField;
+    private JTabbedPane tabbedPanel;
+    private JTextField remoteIpAddressTextField;
     private JButton calculateButton_panel1;
     private JTextField remoteIP1textField;
     private JTextField remoteIP2textField;
     private JButton calculateButton_panel2;
+    private JLabel localIpAddressLabel;
+    private JLabel localCityLabel;
+    private JLabel remoteCityLabel;
     private JLabel distanceLabel_panel1;
+    private JPanel mapPanel_panel1;
+    private JLabel mapLabel_panel1;
+    private JLabel remoteCountryLabel;
+    private JLabel localCountryLabel;
+    private JLabel localASLabel;
+    private JLabel localOrgLabel;
+    private JLabel remoteOrgLabel;
+    private JLabel remoteASLabel;
+    private JTextField sourceIpAddressTextField;
+    private JTextField destinationIpAddressTextField;
+    private JLabel mapLabel_panel2;
+    private JLabel countrySourceLabel;
+    private JLabel citySourceLabel;
+    private JLabel orgSourceLabel;
+    private JLabel asSourceLabel;
+    private JLabel countryDestinationLabel;
+    private JLabel cityDestinationLabel;
+    private JLabel orgDestinationLabel;
+    private JLabel asDestinationLabel;
+    private JLabel distanceLabel_panel2;
+    private JPanel localPanel;
+    private JPanel remotePanel;
+    private JPanel remoteMapPanel;
+    private JLabel mapLabel;
 
 
     @Autowired
-    public MainWindow(GeolocationService geolocationService, DistanceService distanceService, IpValidator ipValidator) {
+    public MainWindow(GeolocationService geolocationService, DistanceService distanceService, IpValidator ipValidator,
+                      MapGenerator mapGenerator) {
         this.geolocationService = geolocationService;
         this.distanceService = distanceService;
         this.ipValidator = ipValidator;
+        this.mapGenerator = mapGenerator;
     }
 
     @PostConstruct
@@ -51,41 +83,102 @@ public class MainWindow extends JFrame {
     }
 
     private void initComponents() {
-        calculateButton_panel1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                String remoteIpAddress = remoteIPtextField.getText();
-                String localIpAddress = GeolocationOperations.getPcIpAddress();
-                if (!ipValidator.validate(rootPanel, remoteIpAddress)) {
-                    Geolocation geolocationLocal = geolocationService.getJsonObject(localIpAddress);
-                    Geolocation geolocationRemote = geolocationService.getJsonObject(remoteIpAddress);
-                    distanceLabel_panel1.setText(distanceService.calculate(geolocationLocal, geolocationRemote));
-                }
-            } catch (IOException e1) {
-                e1.printStackTrace();
+
+        initLocalInformation();
+
+        initDefaultMaps();
+
+        calculateButton_panel1.addActionListener(e ->
+            calculateLocalAction()
+        );
+
+        calculateButton_panel2.addActionListener(e ->
+            calculateRemoteAction()
+        );
+    }
+
+    private void calculateRemoteAction() {
+        String remoteIpAddress1 = sourceIpAddressTextField.getText();
+        String remoteIpAddress2 = destinationIpAddressTextField.getText();
+        calculateRemote(remoteIpAddress1, remoteIpAddress2);
+    }
+
+    private void calculateRemote(String remoteIpAddress1, String remoteIpAddress2) {
+        try {
+            if (!(ipValidator.validate(rootPanel, remoteIpAddress1) ||
+                    ipValidator.validate(rootPanel, remoteIpAddress2))) {
+                Geolocation geolocationRemote1 = geolocationService.getJsonObject(remoteIpAddress1);
+                Geolocation geolocationRemote2 = geolocationService.getJsonObject(remoteIpAddress2);
+                setRemoteInformation(geolocationRemote1, geolocationRemote2);
+            }
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    private void setRemoteInformation(Geolocation geolocationRemote1, Geolocation geolocationRemote2) {
+        distanceLabel_panel2.setText(distanceService.calculate(geolocationRemote1, geolocationRemote2));
+
+        countrySourceLabel.setText(geolocationRemote1.getCountry());
+        citySourceLabel.setText(geolocationRemote1.getCity());
+        orgSourceLabel.setText(geolocationRemote1.getOrg());
+        asSourceLabel.setText(geolocationRemote1.getAs());
+
+        countryDestinationLabel.setText(geolocationRemote2.getCountry());
+        cityDestinationLabel.setText(geolocationRemote2.getCity());
+        orgDestinationLabel.setText(geolocationRemote2.getOrg());
+        asDestinationLabel.setText(geolocationRemote2.getAs());
+
+        mapLabel_panel2.setIcon(mapGenerator.createPathMap(geolocationRemote1, geolocationRemote2));
+    }
+
+    private void calculateLocalAction() {
+        String remoteIpAddress = remoteIpAddressTextField.getText();
+        calculateLocale(remoteIpAddress);
+    }
+
+    private void calculateLocale(String remoteIpAddress) {
+        if (!ipValidator.validate(rootPanel, remoteIpAddress)) {
+            Geolocation geolocationRemote = null;
+            try {
+                geolocationRemote = geolocationService.getJsonObject(remoteIpAddress);
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
-            }
-        });
 
-        calculateButton_panel2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String remoteIpAddres1 = remoteIP1textField.getText();
-                String remoteIpAddres2 = remoteIP2textField.getText();
-                try {
-                    if (!(ipValidator.validate(rootPanel, remoteIpAddres1) ||
-                            ipValidator.validate(rootPanel, remoteIpAddres2)) ) {
-                        Geolocation geolocationRemote1 = geolocationService.getJsonObject(remoteIpAddres1);
-                        Geolocation geolocationRemote2 = geolocationService.getJsonObject(remoteIpAddres2);
-                        distanceService.calculate(geolocationRemote1, geolocationRemote2);
-                    }
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-                }
-        });
+            setLocalInformation(geolocationRemote);
+        }
+    }
+
+    private void setLocalInformation(Geolocation geolocationRemote) {
+        distanceLabel_panel1.setText(distanceService.calculate(geolocationLocal, geolocationRemote));
+        remoteCountryLabel.setText(geolocationRemote.getCountry());
+        remoteCityLabel.setText(geolocationRemote.getCity());
+        remoteOrgLabel.setText(geolocationRemote.getOrg());
+        remoteASLabel.setText(geolocationRemote.getAs());
+        mapLabel_panel1.setIcon(mapGenerator.createPathMap(geolocationLocal, geolocationRemote));
+    }
+
+    private void initDefaultMaps() {
+        mapLabel_panel1.setIcon(mapGenerator.createLocaleMap(geolocationLocal, 14));
+        mapLabel_panel2.setIcon(mapGenerator.createLocaleMap(geolocationLocal, 14));
+    }
+
+    private void initLocalInformation() {
+
+        try {
+            localIpAddress = GeolocationOperations.getPcIpAddress();
+            geolocationLocal = geolocationService.getJsonObject(localIpAddress);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        localIpAddressLabel.setText(localIpAddress);
+        localCountryLabel.setText(geolocationLocal.getCountry());
+        localCityLabel.setText(geolocationLocal.getCity());
+        localASLabel.setText(geolocationLocal.getAs());
+        localOrgLabel.setText(geolocationLocal.getOrg());
     }
 }
---
+++ src/main/java/pl/geolocal/util/MapGenerator.java
@@ -0,0 +1,115 @@
+package pl.geolocal.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import pl.geolocal.domain.impl.Geolocation;
+
+import javax.annotation.PostConstruct;
+import javax.swing.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by piotr on 06.01.2017.
+ */
+@Service
+public class MapGenerator {
+
+    private static final String URL = "http://maps.googleapis.com/maps/api/staticmap?";
+
+    private static final String IMAGE = "C:\\Users\\piotr\\Desktop\\Geolocation_builder\\image.jpg";
+
+    private static final String CENTER = "center";
+    private static final String ZOOM = "zoom";
+    private static final String PATH = "path";
+    private static final String SIZE = "size";
+    private static final String FORMAT = "format";
+    private static final String SCALE = "scale";
+    private static final String KEY = "key";
+
+    private static final String KEY_VALUE = "AIzaSyC_7K2aRloOdnzlrDTUMl6Oc_GRjPRZBhU";
+    private static final String FORMAT_VALUE = "jpg";
+    private static final String SCALE_VALUE = "2";
+    private static final String SIZE_VALUE = "640x640";
+
+    private static final String PATH_PARAMETERS = "color:0x0000ff|weight:5|";
+
+    public Map<String, String> initUrlMap() {
+        Map<String, String> urlMap = new HashMap<>();
+        urlMap.put(SCALE, SCALE_VALUE);
+        urlMap.put(SIZE, SIZE_VALUE);
+        urlMap.put(KEY, KEY_VALUE);
+        urlMap.put(FORMAT, FORMAT_VALUE);
+        return urlMap;
+    }
+
+    public ImageIcon createLocaleMap(Geolocation geolocation, int zoom) {
+        Map<String, String> urlMap = initUrlMap();
+        urlMap.put(CENTER, getLocation(geolocation));
+        urlMap.put(ZOOM, String.valueOf(zoom));
+
+        return createMap(createMapUrl(urlMap));
+    }
+
+
+
+    public ImageIcon createPathMap(Geolocation geolocationSource, Geolocation geolocationDestination) {
+        Map<String, String> urlMap = initUrlMap();
+        String path = getPath(geolocationSource, geolocationDestination);
+        urlMap.put(PATH, path);
+        return createMap(createMapUrl(urlMap));
+    }
+
+    private String getPath(Geolocation geolocationSource, Geolocation geolocationDestination) {
+        return PATH_PARAMETERS + getLocation(geolocationSource) + "|" + getLocation(geolocationDestination);
+    }
+
+    private String getLocation(Geolocation geolocation) {
+        return geolocation.getLat() + "," + geolocation.getLon();
+    }
+
+    private StringBuilder createMapUrl(Map<String, String> urlMap) {
+        StringBuilder imageUrl = new StringBuilder(URL);
+        final Long[] count = {0L};
+        urlMap.forEach((key, value) -> {
+            if (count[0] == 0) {
+                imageUrl.append(key).append("=").append(value);
+            } else {
+                imageUrl.append("&").append(key).append("=").append(value);
+            }
+
+            count[0]++;
+        });
+        return imageUrl;
+    }
+
+    private ImageIcon createMap(StringBuilder imageUrl) {
+        try {
+            String destinationFile = "image.jpg";
+            URL url = new URL(String.valueOf(imageUrl));
+            InputStream is = url.openStream();
+            OutputStream os = new FileOutputStream(destinationFile);
+
+            byte[] b = new byte[2048];
+            int length;
+
+            while ((length = is.read(b)) != -1) {
+                os.write(b, 0, length);
+            }
+
+            is.close();
+            os.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+
+        return new ImageIcon((new ImageIcon(IMAGE)).getImage()
+                .getScaledInstance(630, 600, java.awt.Image.SCALE_SMOOTH));
+    }
+}
